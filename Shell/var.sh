563c5644b8bcd26fd58cb8dbef03c936fe33cfd9
Muhamed Marshad <muhamedmarshad18@gmail.com>
2024-07-10 22:03:49 +0530

    Add file

--- Shell/var.sh
+++ Shell/var.sh
@@ -1,48 +1,14 @@
-# #!/bin/bash
-# echo "Script name: $0"
-# echo "Number of arguments: $#"
-# echo "All arguments: $@"
-# echo "PID of the script: $$"
-
-# if [ $# -gt 0 ]; then
-#     echo "First argument: $1"
-# fi
-
-# # Run a background command
-# sleep 10 &
-# echo "PID of last background command: $!"
-
-# # Check exit status of a command
-# ls /nonexistent
-# echo "Exit status of the last command: $?"
-
-# # Using $_
-# echo "Previous argument: $_"
 #!/bin/bash
+echo "Script name: $0"
+echo "Number of arguments: $#"
+echo "PID of the script: $$"
 
-isAmstrong() {
-    num=$1
-    sum=0
-    original_num=$num
-
-    while (( num > 0 ))
-    do
-        digit=$(( num % 10 ))
-        sum=$(( sum + digit * digit * digit ))
-        num=$(( num / 10 ))
-    done
-
-    echo $sum
-}
-
-# Function call
-read -p "Enter a number: " num
-am=$(isAmstrong $num)
-
-# Check if the number is an Armstrong number
-if [ $am -eq $num ]
-then
-    echo "Armstrong number"
-else
-    echo "Not an Armstrong number!"
+if [ $# -gt 0 ]; then
+    echo "First argument: $1"
 fi
+
+# Run a background command
+sleep 10 &
+echo "PID of last background command: $!"
+
+echo "Previous argument: $_"
